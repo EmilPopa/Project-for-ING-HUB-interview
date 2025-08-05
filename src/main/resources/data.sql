3da052b49bc20902baf3670b91a872ad539079f2
Emil Popa <emilpopa24@yahoo.com>
2025-08-05 22:52:36 +0300

    Create schema.sql and data.sql files

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO category(id, name) VALUES (1, 'Electronics');
+INSERT INTO category(id, name) VALUES (2, 'Books');
+INSERT INTO category(id, name) VALUES (3, 'Clothing');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,4 @@
+CREATE TABLE category (
+    id BIGINT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
